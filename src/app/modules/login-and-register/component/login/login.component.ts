f48692e504f247c241b9c77780f47c6607235853
Paweł Wasilonek <pawel.wasilonek@polcode.net>
2021-07-26 15:59:17 +0200

    feature: add validation to inputs

--- src/app/modules/login-and-register/component/login/login.component.ts
+++ src/app/modules/login-and-register/component/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  fg!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  login(): void {
+    if (this.fg.invalid) {
+      return;
+    }
+
+    console.log('login credentials', this.fg.getRawValue()); // todo write services
+  }
+
+  private initForm(): void {
+    this.fg = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 }
--- src/app/modules/login-and-register/component/register/register.component.ts
+++ src/app/modules/login-and-register/component/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
+  fg!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private readonly fb: FormBuilder) {}
+
+  register(): void {
+    if (this.fg.invalid) {
+      return;
+    }
+
+    console.log('register data', this.fg.getRawValue());
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.fg = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 }
