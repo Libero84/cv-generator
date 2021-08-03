200d3a646bfc70f869968deb22c0da2782f4d057
Paweł Wasilonek <pawel.wasilonek@polcode.net>
2021-08-03 12:28:15 +0200

    feature: add static input with main information

--- src/app/modules/cv/component/add-cv/add-cv.component.ts
+++ src/app/modules/cv/component/add-cv/add-cv.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CvService } from '../../service/cv.service';
 
 @Component({
   selector: 'app-add-cv',
@@ -6,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-cv.component.scss'],
 })
 export class AddCvComponent implements OnInit {
-  constructor() {}
+  fg!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private readonly cvService: CvService, private readonly formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  addCv(): void {
+    if (this.fg.invalid) {
+      return;
+    }
+  }
+
+  cancel(): void {}
+
+  private initForm(): void {
+    this.fg = this.formBuilder.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: [null, Validators.required],
+    });
+  }
 }
