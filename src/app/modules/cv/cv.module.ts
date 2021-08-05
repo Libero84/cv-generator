57e72b76c1dc75d1cd576bc53d07757e5aabfa32
Paweł Wasilonek <pawel.wasilonek@polcode.net>
2021-08-05 07:55:46 +0200

    feature: add dynamic form Array to add cvs

--- src/app/modules/cv/cv.module.ts
+++ src/app/modules/cv/cv.module.ts
@@ -7,7 +7,6 @@ import { MatTableModule } from '@angular/material/table';
 import { CvComponent } from './cv.component';
 import { MatIconModule } from '@angular/material/icon';
 import { AddCvComponent } from './component/add-cv/add-cv.component';
-import { UpdateCvGenerationComponent } from './component/update-cv-generation/update-cv-generation.component';
 import { DetailsCvComponent } from './component/details-cv/details-cv.component';
 import { EducationComponent } from './component/education/education.component';
 import { ExperiencesComponent } from './component/experiences/experiences.component';
@@ -17,14 +16,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    CvComponent,
-    AddCvComponent,
-    UpdateCvGenerationComponent,
-    DetailsCvComponent,
-    EducationComponent,
-    ExperiencesComponent,
-  ],
+  declarations: [CvComponent, AddCvComponent, DetailsCvComponent, EducationComponent, ExperiencesComponent],
   imports: [
     CommonModule,
     CvRoutingModule,
